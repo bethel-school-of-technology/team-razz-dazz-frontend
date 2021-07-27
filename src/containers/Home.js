6064ffe5a9cfac921a697f829396f41e4deb97e5
Steven Searcy <stevenaaronsearcy@gmail.com>
2021-07-27 18:03:53 -0500

    Folder additions

---
+++ src/containers/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeTitle from "../components/HomeTitle";
+
+function Home () {
+    return (
+        <div>
+        <HomeTitle />
+        </div>
+    )
+}
+
+export default Home;
