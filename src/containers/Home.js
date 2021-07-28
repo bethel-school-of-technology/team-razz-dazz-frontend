3c08fb8e6ea0d6a12824e7baca8ef8af2720c485
Steven Searcy <stevenaaronsearcy@gmail.com>
2021-07-28 17:36:47 -0500

    S3 Bucket upload created

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeTitle from "../components/HomeTitle";
 import Container from "react-bootstrap/Container";
+import S3Upload from "../components/S3Upload";
 
 const Home = () => {
     return (
       <Container fluid>
         <div>
           <HomeTitle />
+          <S3Upload />
         </div>
       </Container>
     );
