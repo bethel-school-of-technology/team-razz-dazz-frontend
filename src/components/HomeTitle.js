edf0f7e3e702d4d3ee300274770a10647f5f14a9
Chad Crawford <ckcrawford81@gmail.com>
2021-08-01 12:40:25 -0500

    header

--- src/components/HomeTitle.js
+++ src/components/HomeTitle.js
@@ -1,17 +1,45 @@
-import React from "react";
-import "../components/HomeTitle.css";
-import { MDBBtn } from "mdb-react-ui-kit";
+import React from 'react';
+import "../components/HomeTitle.css"
 
 
 const HomeTitle = () => {
   return (
-    <div>
-      <h1>Home stuffssss goes here</h1>
-      <MDBBtn gradient="blue">Already A Noble Cookie Addict? Click to Login</MDBBtn>
-      <MDBBtn gradient="peach">New to The Noble Cookie Experience? Sign Up Here!</MDBBtn>
-    </div>
-  );
-};
 
+<div
+        id='intro-example'
+        className='p-5 text-center bg-image'
+      >
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-white'>
+              <h1 className='mb-3'>Learn Bootstrap 5 with MDB</h1>
+              <h5 className='mb-4'>Best &amp; free guide of responsive web design</h5>
+              <a
+                className='btn btn-outline-light btn-lg m-2'
+                href='https://www.youtube.com/watch?v=c9B4TPnak1A'
+                role='button'
+                rel='nofollow'
+                target='_blank'
+              >
+                Start tutorial
+              </a>
+              <a
+                className='btn btn-outline-light btn-lg m-2'
+                href='https://mdbootstrap.com/docs/standard/'
+                target='_blank'
+                role='button'
+              >
+                Download MDB UI KIT
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+  );
+}
 
 export default HomeTitle;
+
+{/* <MDBBtn id="homebutton1">Already A Noble Cookie Addict? Click to Login</MDBBtn>
+<MDBBtn id="homebutton2">New to The Noble Cookie Experience? Sign Up Here!</MDBBtn> */}
