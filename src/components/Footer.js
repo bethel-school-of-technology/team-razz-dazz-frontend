e2bfbe62e2d9a01cf565616f06d7dd899bafcceb
MathewLukacs <lukacsmathew@gmail.com>
2021-08-03 17:56:56 -0400

    get some mr. footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,50 +1,83 @@
-import React from "react";
-import { MDBFooter, MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
-import "../components/Footer.css";
+import React from 'react';
+import { MDBFooter, MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 
-const Footer = () => {
+export default function App() {
   return (
-    <MDBFooter className="text-center text-lg-left" id="footer">
-      <MDBContainer className="p-4">
+    <MDBFooter bgColor='primary' className='text-white text-center text-lg-left'>
+      <MDBContainer className='p-4'>
         <MDBRow>
-          <MDBCol lg="6" md="12" className="mb-4 mb-md-0">
-            <h5 className="text-uppercase">Footer text</h5>
+          <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
+            <h5 className='text-uppercase'>Footer Content</h5>
 
             <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-              atque ea quis molestias. Fugiat pariatur maxime quis culpa
-              corporis vitae repudiandae aliquam voluptatem veniam, est atque
-              cumque eum delectus sint!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis molestias.
+              Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam, est
+              atque cumque eum delectus sint!
             </p>
           </MDBCol>
 
-          <MDBCol lg="6" md="12" className="mb-4 mb-md-0">
-            <h5 className="text-uppercase">Footer text</h5>
+          <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+            <h5 className='text-uppercase'>Links</h5>
 
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-              atque ea quis molestias. Fugiat pariatur maxime quis culpa
-              corporis vitae repudiandae aliquam voluptatem veniam, est atque
-              cumque eum delectus sint!
-            </p>
+            <ul className='list-unstyled mb-0'>
+              <li>
+                <a href='#!' className='text-white'>
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a href='#!' className='text-white'>
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a href='#!' className='text-white'>
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a href='#!' className='text-white'>
+                  Link 4
+                </a>
+              </li>
+            </ul>
+          </MDBCol>
+
+          <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+            <h5 className='text-uppercase mb-0'>Links</h5>
+
+            <ul className='list-unstyled'>
+              <li>
+                <a href='#!' className='text-white'>
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a href='#!' className='text-white'>
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a href='#!' className='text-white'>
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a href='#!' className='text-white'>
+                  Link 4
+                </a>
+              </li>
+            </ul>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
 
-      <div
-        className="text-center p-3"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-      >
-        &copy; {new Date().getFullYear()} Copyright:{" "}
-        <a className="text-dark" href="https://mdbootstrap.com/">
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        &copy; {new Date().getFullYear()} Copyright:{' '}
+        <a className='text-white' href='https://mdbootstrap.com/'>
           MDBootstrap.com
         </a>
       </div>
     </MDBFooter>
   );
 }
-
-export default Footer;
-
-
-
