dd3ae3f97f64264cf903d89514f8fb8f530424fd
Steven Searcy <stevenaaronsearcy@gmail.com>
2021-08-06 16:47:11 -0500

    text issues

--- src/components/HomeTitle.js
+++ src/components/HomeTitle.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../components/HomeTitle.css";
-import { MDBBtn } from "mdb-react-ui-kit";
+import { MDBBtn, MDBTypography } from "mdb-react-ui-kit";
+
 
 
 const HomeTitle = () => {
@@ -9,10 +10,10 @@ const HomeTitle = () => {
       <div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}>
         <div className="d-flex justify-content-center align-items-center h-100">
           <div className="text-white">
-            <h1 className="mb-3">The Noble Cookie</h1>
-            <h5 className="mb-4">
+            <MDBTypography tag='h1' className="mb-3">The Noble Cookie</MDBTypography>
+            <MDBTypography tag="h1" className="mb-4">
               Beautifully designed, carefully crafted custom cookies so good, they'd make your grandma weep with jealousy!
-            </h5>
+            </MDBTypography>
 
             <MDBBtn id="homebutton1" className="btn btn-outline-light btn-lg m-2" href="/login">
               Already A Noble Cookie Addict? Click to Login
