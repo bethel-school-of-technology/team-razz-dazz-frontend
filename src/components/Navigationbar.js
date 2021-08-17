439b94eff52ccc2fb136e6f2a5051c5ac4910a17
Steven Searcy <stevenaaronsearcy@gmail.com>
2021-08-17 17:17:58 -0500

    cheese

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -14,14 +14,12 @@ import "./Navigationbar.css";
 import Cookie from "../assets/cookielogo.png";
 
 
-const Navigationbar = ({history}) => {
+const Navigationbar = ({history}, routerProps) => {
   const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [isAuthenticated, userHasAuthenticated] = useState(false);
   const token = localStorage.getItem("myJWT"); 
   const [showNavText, setShowNavText] = useState(false);
 
-
-  
   
   useEffect(() => {
     async function onLoad() {
@@ -156,48 +154,5 @@ const Navigationbar = ({history}) => {
     )
   );
 }
-
-
-// const Navigationbar = () => {
-//   return (
-//     <Navbar collapseOnSelect expand="lg" variant="dark" id="navbar">
-//       <Container>
-//         <Navbar.Brand href="/">
-//           <img src={Cookie} alt="cookie" id="cookie" href="/" />
-//         </Navbar.Brand>
-//         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//         <Navbar.Collapse id="responsive-navbar-nav">
-//           <Nav className="me-auto">
-//             {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-//               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-//               <NavDropdown.Item href="#action/3.2">
-//                 Another action
-//               </NavDropdown.Item>
-//               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-//               <NavDropdown.Divider />
-//               <NavDropdown.Item href="#action/3.4">
-//                 Separated link
-//               </NavDropdown.Item>
-//             </NavDropdown> */}
-//           </Nav>
-//           <Nav>
-//             <Nav.Link href="/gallery" id="linktext">
-//               Gallery
-//             </Nav.Link>
-//             <Nav.Link href="/orderpage" id="linktext">
-//               Order Now
-//             </Nav.Link>
-//             <Nav.Link href="/login" id="linktext">
-//               Login
-//             </Nav.Link>
-//             <Nav.Link eventKey={2} href="/signup" id="linktext">
-//               Sign up
-//             </Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// };
 
 export default Navigationbar;
