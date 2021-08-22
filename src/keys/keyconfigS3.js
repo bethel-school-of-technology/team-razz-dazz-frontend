7bf83c69e39ba2cce43963f4506cd08e5ed40b2e
Chad Crawford <ckcrawford81@gmail.com>
2021-08-22 13:48:26 -0500

    attempt to change footer and make it sticky in viewport, unsuccessful but shows i tried

---
+++ src/keys/keyconfigS3.js
@@ -0,0 +1,9 @@
+const config = {
+  bucketName: "noblecookie",
+  dirName: "usersubmissions",
+  region: "us-east-1",
+  accessKeyId: "AKIARRDBSFQ5ADITPU67",
+  secretAccessKey: "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",
+};
+
+export default config;
